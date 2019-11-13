0718d306a119f60839ed86697b844036359768bc
corneloup theo <theo.corneloup42@gmail.com>
2019-11-13 16:07:11 +0100

    Intialisation

---
+++ src/test/java/com/fr/adaming/service/USerServiceTest.java
@@ -0,0 +1,16 @@
+package com.fr.adaming.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class USerServiceTest {
+
+	@Autowired
+	private UserService service;
+	
+	public void createValidUSer_shouldReturnUserNotNull() {
+		
+	}
+	
+}
