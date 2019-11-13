8e5085d270a074255e3df7b8b3c4836adc7c268e
corneloup theo <theo.corneloup42@gmail.com>
2019-11-13 16:30:12 +0100

    modification test unitaire

--- src/test/java/com/fr/adaming/service/USerServiceTest.java
+++ src/test/java/com/fr/adaming/service/USerServiceTest.java
@@ -1,15 +1,36 @@
 package com.fr.adaming.service;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fr.adaming.entity.User;
+
 @SpringBootTest
-public class USerServiceTest {
+public class UserServiceTest {
 
 	@Autowired
 	private UserService service;
 	
+	@Test
 	public void createValidUSer_shouldReturnUserNotNull() {
+		
+		//preparer input
+		User u = new User();
+		
+		
+		u.setNom("NomA");
+		u.setEmail("adming@adaming.fr");
+		u.setPwd("azerty");
+		
+		// invoquer la methode
+		User returnedUser = service.save(u);
+		
+		//verifier le retour de la méthode
+		assertNotNull(returnedUser);
+		assertNotNull(returnedUser.getId());
 		
 	}
 	
